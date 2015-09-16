9e10fc8c9c211197d4ab5ce01a84c5c0d1b5510c
darkneo14 <prashant.gupta1194@gmail.com>
2015-09-16 19:40:33 +0530

    An basic Android app
    A basic android app including all the basic components...

---
+++ src/com/thenewboston/travis/GFX.java
@@ -0,0 +1,36 @@
+package com.thenewboston.travis;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+
+public class GFX extends Activity{
+
+	MyBringBack ourView;
+	WakeLock wL;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		
+		//Wake Lock
+		PowerManager pM=(PowerManager)getSystemService(Context.POWER_SERVICE);
+		wL= pM.newWakeLock(PowerManager.FULL_WAKE_LOCK, "Yo");
+		
+		super.onCreate(savedInstanceState);
+		wL.acquire();
+		ourView=new MyBringBack(this);
+		setContentView(ourView);
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		wL.release();
+	}
+	
+
+}
